ee604d1b665a8825ed14ef533294b64973563975
puja khadka <khadkapuja195@gmail.com>
2026-01-03 16:19:58 +0545

    new div is added inside home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar'
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import { useEffect } from 'react';
 
 function Home() {
+    useEffect(() => {
+    AOS.init({
+      duration: 1000, 
+      offset: 100,    
+      easing: 'ease', 
+      once: true,     
+    });
+  }, []);
+
   return (
       <>
        <NavBar/>
@@ -14,18 +26,42 @@ function Home() {
     backgroundRepeat: "no-repeat",
         }}>
 
-          <div className='pt-20 mx-70'>
+          <div className='pt-20 mx-70 '>
+            <span data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine">
             <h3  className='text-green-500 text-center font-bold text-xl'>Best IT Solution</h3>
-            <h1 className='text-7xl text-center font-bold text-black'>Quality Digital <br/>Services You Really <br/>Need</h1>
+            <h1 className='text-7xl text-center font-bold text-black'>Quality Digital <br/>Services You Really <br/>Need</h1> </span>
             <p className='text-center my-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat id adipisci ex nam sunt quo minima autem nostrum, ut magni!</p>
-            <div className='flex justify-center gap-10 mt-6'>
-              <button className='rounded-full bg-green-600 px-8 py-3'>Read More</button>
-              <button className='rounded-full bg-blue-600 px-8 py-3 text-white'>Contact Us</button>
+            <div className='flex justify-center gap-8 mt-6'>
+              <button className='rounded-full bg-green-600 px-8 py-3' data-aos="fade-right">Read More</button>
+              <button className='rounded-full bg-blue-600 px-8 py-3 text-white' data-aos="fade-left">Contact Us</button>
             </div>
           </div>
          
 
       </section>
+      <div className='bg-blue-600 py-15' >
+        <div className=' container mx-auto flex items-center justify-between gap-4 px-20' >
+             <div className='flex items-center gap-3' data-aos="zoom-in">
+                <span className='text-4xl text-blue-300 font-bold'>99</span>
+                <h1 className='text-xl text-white font-bold'>Success in getting happy customer </h1>
+             </div>
+             <div className='flex items-center gap-3' data-aos="zoom-in">
+                <span className='text-4xl text-blue-300 font-bold'>99</span>
+                <h1 className='text-xl text-white font-bold'>Success in getting happy customer </h1>
+             </div>
+             <div className='flex items-center gap-3 font-bold' data-aos="zoom-in">
+                <span className='text-4xl text-blue-300 font-bold'>99</span>
+                <h1 className='text-xl text-white'>Success in getting happy customer </h1>
+             </div>
+             <div className='flex items-center gap-3' data-aos="zoom-in">
+                <span className='text-4xl text-blue-300 font-bold'>99</span>
+                <h1 className='text-xl text-white font-bold'>Success in getting happy customer </h1>
+             </div>
+        </div>
+      </div>
+     
       </>
   )
 }
